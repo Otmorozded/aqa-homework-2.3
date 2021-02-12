bfdda65935a172273915e120ae206132ebf210c9
Evgeny Vasilev <jony.torrent@gmail.com>
2021-02-12 12:29:34 +0300

    initial commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+public class PostmanTest {
+    @Test
+    void shouldReturnPostRequest() {
+        //Подход Given-When-Then
+        //Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                //Выполняемые действия
+                .when()
+                .post("/post")
+                //Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some name"));
+
+    }
+}
