fddc5fbbcb7be8a5208438c127521449c69abf5d
Evgeny Vasilev <jony.torrent@gmail.com>
2021-02-12 12:37:51 +0300

    test fix

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -20,7 +20,7 @@ public class PostmanTest {
                 //Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some name"));
+                .body("data", equalTo("some data"));
 
     }
 }
